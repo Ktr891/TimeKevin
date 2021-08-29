a2ae429defe4dbb5d789afdca11284ab6ee080ba
KEVIN TORRES RIVERA <kevintorres259397@correo.itm.edu.co>
2021-08-29 14:20:31 -0500

    Add entities

---
+++ timeKevin.Function/Entities/TimeEntity.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace timeKevin.Function.Entities
+{
+    public class TimeEntity : TableEntity
+    {
+        public int IdEmployee { get; set; }
+        public DateTime EntryDate { get; set; }
+        public string Type { get; set; }
+        public bool Consolidate { get; set; }
+    }
+}
---
+++ todoTime.Common/Models/Time.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace timeKevin.Common.Models
+{
+    public class Time
+    {
+        public int IdEmployee { get; set; }
+        public DateTime EntryDate { get; set; }
+        public string Type { get; set; }
+        public bool Consolidate { get; set; }
+    }
+}
